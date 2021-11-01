3bd5581c5efe573f07f477ca182d1453f27e4afe
Ashen Nilura <52426498+Nilura@users.noreply.github.com>
2021-11-01 14:51:57 +0530

    Add files via upload

---
+++ backend_node/config/dbconfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: 'yoursecret',
+    database: 'mongodb+srv://tutorial:xP9mB3ZDfD37ukCj@mongodb-tutorial.g9gjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ backend_node/models/expert.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt')
+var userSchema1 = new Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    
+designation:{
+    type: String,
+    require: true
+},
+contact:{
+    type: Number,
+    require: true
+},
+location:{
+    type: String,
+    require: true
+},
+    password: {
+        type: String,
+        require: true
+    }
+})
+
+userSchema1.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err)
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err)
+                }
+                user.password = hash;
+                next()
+            })
+        })
+    }
+    else {
+        return next()
+    }
+})
+
+userSchema1.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if(err) {
+            return cb(err)
+        }
+        cb(null, isMatch)
+    })
+}
+
+module.exports = mongoose.model('expert', userSchema1)
