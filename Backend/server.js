27dda88bcd044b723e4d863ec7441f7321853b65
Ashen Nilura <52426498+Nilura@users.noreply.github.com>
2021-11-12 07:23:19 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,27 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const routes = require('./routes/index')
 
-//Read & Load env vars to process.env
-dotenv.config({ path: "./config/config.env" });
+connectDB()
 
-const PORT = process.env.PORT || 5000;
+const app = express()
 
-const app = express();
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(routes)
+app.use(passport.initialize())
+require('./config/passport')(passport)
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
