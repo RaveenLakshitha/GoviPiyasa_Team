a3a7d8b787a5f674074746c53258ee2b47d63ec2
Nethmi Warnakula <42459272+nethmi1234@users.noreply.github.com>
2021-11-15 20:46:07 +0530

    Update User.js

--- AdminApp/src/components/User.js
+++ AdminApp/src/components/User.js
@@ -16,15 +16,7 @@ const User = () => {
     <div className="m-1">
       <div className="row">
         <div className="col-6">
-        {/* <div className="input-group">
-          <div className="form-outline">
-            <input type="search" id="form1" className="form-control" />
-            <label class="form-label" for="form1">Search</label>
-          </div>
-          <button type="button" className="btn btn-primary">
-            <i className="fas fa-search"></i>
-          </button>
-        </div> */}
+     
           <input type="text" placeholder="Search..."/>
         </div>
         
